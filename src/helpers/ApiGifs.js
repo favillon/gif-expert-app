1771d81edb621a24c7ad2322cbc4f12afbbf92b2
favillon <favillon@hotmail.com>
2023-01-26 12:16:26 -0500

    Correccion url  y creacion carpeta docs

--- src/helpers/ApiGifs.js
+++ src/helpers/ApiGifs.js
@@ -2,7 +2,7 @@ const apiKey = 'xwMITpgwEovEJ9ZnwxrsC8SaNFH00fg9';
 const limit = 20;
 
 export const getGifs = async(category) => {
-  const url      = `http://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${category}&limit=${limit}`;
+  const url      = `https://api.giphy.com/v1/gifs/search?api_key=${ apiKey }&q=${category}&limit=${limit}`;
   const resp     = await fetch(url)
   const { data } = await resp.json()
   const gifs = data.map( img =>({
